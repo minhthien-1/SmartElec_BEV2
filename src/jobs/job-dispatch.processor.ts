4a0646d3bf7893039d42434805f859179afcaf26
TPaooo1806 <23dh110242@st.huflit.edu.vn>
2026-08-22 13:44:59 +0700

    fix: PostgreSQL HAVING clause syntax error in findNearbyTechnicians

--- src/jobs/job-dispatch.processor.ts
+++ src/jobs/job-dispatch.processor.ts
@@ -175,7 +175,7 @@ export class JobDispatchProcessor extends WorkerHost {
         AND "isOnline" = true 
         AND latitude IS NOT NULL 
         AND longitude IS NOT NULL
-      HAVING (6371 * acos(
+        AND (6371 * acos(
           LEAST(1.0, 
             cos(radians(${lat})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${lng})) + 
             sin(radians(${lat})) * sin(radians(latitude))
